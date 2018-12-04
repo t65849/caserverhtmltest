084ff8171b6ccded1cafe5cabde0df4d06b0b7a7
ETATUNG\Andy.Wu <t658493213@gmail.com>
2018-12-04 15:32:13 +0800

    test

--- app.js
+++ app.js
@@ -127,8 +127,7 @@ app.post('/indexpage', function(req, res){
                 console.info(body);
             });
         });
-        res.writeHead({Location: 'https://caserverhtmltest.herokuapp.com/indexpage'});
-        res.end();
+        res.redirect('/indexpage');
     }
 })
 
